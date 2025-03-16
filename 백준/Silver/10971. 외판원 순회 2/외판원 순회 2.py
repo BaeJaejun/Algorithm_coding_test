465cc8b1b18f6769152b2bd97519f121a90f79e6
BaeJaejun <a01072004146@gmail.com>
2025-03-17 01:09:12 +0900

    [Silver II] Title: 외판원 순회 2, Time: 6008 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
@@ -14,19 +14,19 @@ min_cost = float('inf')
 
 for x in permutations(N_list): #순열 조합 하나에서 이동하는 경우 하나 뽑기
     cost = 0  
-    visited = [False for _ in range(N)] # 이동 여부를 판별 N개 마을 이 있으면 N번 움직임
-    
+    check = 0
     for y in range(N):
         c = W[ x[y%(N)] ][ x[(y+1)%N] ]
         if  c == 0:  
+            check = 1
             break
         else:
-            visited[x[y]] = True
             cost += c
             if cost >= min_cost:
+                check = 1
                 break
                 
-    if False not in visited and min_cost > cost:
+    if check == 0 and min_cost > cost:
         min_cost = cost
     
     
