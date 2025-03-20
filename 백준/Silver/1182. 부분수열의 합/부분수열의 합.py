2aea3563260de3317abd9ca46ea8f2d6cea09e2b
BaeJaejun <a01072004146@gmail.com>
2025-03-20 14:36:39 +0900

    [Silver II] Title: 부분수열의 합, Time: 436 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -7,7 +7,7 @@ N,S = map(int,input().split())
 n_list = list(map(int,input().split()))
 sum_list = []
 sol_list = set()
-visited = [False] * N
+#visited = [False] * N
 
 def sol(s,idx):
     global cnt
@@ -17,18 +17,18 @@ def sol(s,idx):
         
     
     for i in range(idx, len(n_list)):
-        if visited[i] == False:
-            sum_list.append(n_list[i])
-            visited[i] = True
-            sol(s,i)
-            visited[i] = False
-            sum_list.pop()
+        #if visited[i] == False:
+        sum_list.append(n_list[i])
+        #   visited[i] = True
+        sol(s,i+1)
+        #  visited[i] = False
+        sum_list.pop()
 
 
 cnt = 0
 
 sol(S,0) # 순열뽑기 조합으로 어떻게? 이차원배열에서 중복제거?
-
+        # idx 현재 인덱스를 추가해서 다음 배열에서 배열 시작지점을 골라줌
 print(cnt)
 #print(sol_list)
 #print(len(sol_list))
