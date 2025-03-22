5bcf3787ee305d0d7bbd20c7dc3515fb139d84fe
BaeJaejun <a01072004146@gmail.com>
2025-03-22 11:22:53 +0900

    [Gold V] Title: 두 용액, Time: 100 ms, Memory: 44748 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.py
+++ 백준/Gold/2470. 두 용액/두 용액.py
@@ -1,50 +1,27 @@
 import sys
-from itertools import combinations
+
 input = sys.stdin.readline
 
 N = int(input().strip())
 
-liquid = list(map(int,input().split()))
+liquids = list(map(int,input().split()))
+liquids.sort()
 
-liquid.sort()
-
-# 완전 탐색 O(n^2) -> 느림
-# result = float('inf')
-# x,y = 0,0
-# for i in range(N-1):
-#     for j in range(i+1,N):
-#         sumL = liquid[i] + liquid[j]
-           
-#         if result > abs(sumL):
-#             x,y = i,j
-#             result = abs(sumL)
-# print(f'{liquid[x]} {liquid[y]}',end="")
-
-# 이분탐색
+pl = 0
+pr = len(liquids) - 1
 
 zerozero = float('inf')
-x,y = 0,0
-for i in range(N):
-    #값 한개 고정
-    a = liquid[i]
-    #값에 역수에 가장 가까운 값을 나머지배열에서 이진탐색으로 찾기
-    target = -a
-    
-    left = i+1
-    right = N-1
-    while left <= right:
-        mid = (left + right)//2
-        b = liquid[mid]
-        sumL = a + b
-        
-        if abs(sumL) < abs(zerozero):
-            zerozero = sumL
-            x,y = a, b    
-        
-        if sumL < 0 :
-            left = mid + 1
-        else:
-            right = mid - 1
-            
 
-print(x, y)
+while pl < pr:
+    sumL = liquids[pl] + liquids[pr]
+
+    if abs(sumL) < abs(zerozero):
+        zerozero = sumL
+        x = pl
+        y = pr
+    if sumL < 0:
+        pl += 1
+    else:
+        pr -= 1
+        
+print(liquids[x],liquids[y])
