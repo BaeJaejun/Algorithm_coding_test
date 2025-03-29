ff6bcc769d40d2daa8116015765419f01a2acbf5
BaeJaejun <a01072004146@gmail.com>
2025-03-30 01:00:31 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 160 ms, Memory: 36504 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -6,62 +6,49 @@ N = int(input().strip())
 
 A = list(map(int,input().split()))
 
-# + - * /
-op = list(map(int,input().split()))
-real_op = [] 
-
-
-for i in range(4): # 연산자 4개임
-    for j in range(op[i]):
-        if i == 0:
-            real_op.append('+')
-        elif i == 1:
-            real_op.append('-')
-        elif i == 2:
-            real_op.append('*')
-        elif i == 3:
-            real_op.append('/')
-
+x = ['+', '-', '*', '/']
+op1 = list(map(int,input().split()))
 max_result = -float('inf')
 min_result = float('inf')
-op_list = []
-visited = [False] * len(real_op)
-def recursion(depth,op_len,current):
-    if depth == op_len:
-        op_list.append(current[:])
-        #print(' '.join(map(str,current)))
-        return 
 
-    for i in range(op_len):
-        if not visited[i]:
-            visited[i] = True
-            recursion(depth+1,op_len,current + [real_op[i]])
-            visited[i] = False    
+op = []
+for i in range(4):
+    op += [x[i]] * op1[i]
+
+
+result = []
+visited = [False] * len(op)
+def per(depth, n, current):
+    if depth == n:
+        result.append(current[:])
+        return
     
-recursion(0,len(real_op),[])
+    prev = None
+    for i in range(n):
+        if not visited[i] and op[i] != prev:
+            visited[i] = True
+            per(depth+1, n,current + [op[i]])
+            visited[i] = False
+            prev = op[i]
 
-#중복 연산자가 있을 때 생길 중복 순열을 제거
+op.sort()
+per(0,len(op),[])
 
-set_op_list = set(tuple(op) for op in op_list)
-
-no_dup_op_list = list(list(op) for op in set_op_list)
-
-
-for i in range(len(no_dup_op_list)):
+for i in range(len(result)):
     value = A[0]
-    for a in range(len(A)-1):
-        if no_dup_op_list[i][a] =="+":
-            value +=  A[a+1]
-        elif no_dup_op_list[i][a] =="-":
+    for a in range(len(op)):
+        if result[i][a] == '+':
+            value += A[a+1]
+        elif result[i][a] == '-':
             value -= A[a+1]
-        elif no_dup_op_list[i][a] =="*":
+        elif result[i][a] == '*':
             value *= A[a+1]
-        elif no_dup_op_list[i][a] =="/":
+        elif result[i][a] == '/':
             if value < 0:
-                value = -(-value // A[a+1])
+                value = -1*((value * -1) // A[a+1])
             else:
                 value = value // A[a+1]
-    
+        
     if max_result < value:
         max_result = value
     if min_result > value:
