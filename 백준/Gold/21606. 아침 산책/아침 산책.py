302023cdb196fd62a94ea3d5e52305face7c3cd2
BaeJaejun <a01072004146@gmail.com>
2025-04-02 16:08:33 +0900

    [Gold III] Title: 아침 산책, Time: 824 ms, Memory: 107196 KB, Score: 200 point -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.py
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -2,36 +2,52 @@ import sys
 sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
-N = int(input().strip())
-A =[-1]
+
+n = int(input().strip())
+A = [0]
 A = A + list(map(int,input().strip()))
 
+graph = [[] for _ in range(n+1)]
 
-graph = [[] for _ in range(N+1)]
-
-for i in range(1,N):
+for _ in range(n-1):
     u,v = map(int,input().split())
+   
     graph[u].append(v)
     graph[v].append(u)
 
-cnt = 0
-def dfs(start):
-    global cnt
-    visited[start] = True
-    
-    for i in graph[start]:
-        if visited[i] == False:
-            if A[i] == 1:
-                cnt +=1
-                continue
-            else:
-                dfs(i)
+visited = [False for _ in range(n+1)]
+total_cnt = 0
 
-for j,k in enumerate(A):
-    visited = [False] *(N+1)
-    if k == -1:
-        pass
-    if k == 1:
-        dfs(j)
+# 흰색 노드 체크
+def dfs(i): # cnt == 흰색 주변 검은색 개수
+    visited[i] = True
+    black_cnt = 0
+    # 흰색 주변에 최소 2개의 검은색이 있어야 cnt 가 올라감
+    for x in graph[i]:
+        if visited[x] == False:
+            if A[x] == 1:
+                black_cnt += 1         
+            else:
+                black_cnt += dfs(x)
+          
+    return black_cnt 
         
-print(cnt)
+             
+             
+for i in range(1,n+1):
+    if not visited[i] and A[i] == 0:
+        for j in graph[i]:
+            #다른 덩어리의 흰색 노드 주변을 false로 바꿔주기
+            if visited[j] == True:
+                visited[j] = False
+        x = dfs(i)
+        total_cnt += x * (x-1)
+        
+
+#검은색 노드 체크
+for i in range(1, n + 1):
+    if A[i] == 1:
+        for j in graph[i]:
+            if A[j] == 1:
+                total_cnt += 1   
+print(total_cnt)
