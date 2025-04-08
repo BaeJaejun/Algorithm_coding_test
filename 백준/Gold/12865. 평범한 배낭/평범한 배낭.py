b76d5a2454e84449cbf62c47592ae48da60d2221
BaeJaejun <a01072004146@gmail.com>
2025-04-08 11:34:59 +0900

    [Gold V] Title: 평범한 배낭, Time: 2460 ms, Memory: 36264 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -2,27 +2,30 @@ import sys
 
 input = sys.stdin.readline
 
-n,k = map(int,input().split())
+N, K = map(int,input().split())
 
-things = []
-for _ in range(n):
+item = []
+for _ in range(N):
     w,v = map(int,input().split())
-    things.append((w,v))
+    
+    item.append((w,v))
     
 
-# dp[i][w]: i번째 물건까지 고려했을 때, 무게 w일 때의 최대 가치
-# 이전 물건(i-1) 기준으로,
-# 안 넣으면: dp[i-1][w]
-# 넣으면: dp[i-1][w - weight] + value
-# 두 값 중 max()로 더 큰 걸 선택해서 갱신
-dp = [[0 for _ in range(k+1)] for _ in range(n+1)]
+dp = [0 for _ in range(K+1)]
 
-for i in range(1,n+1):
-    weight, value = things[i-1]
-    for j in range(1,k+1):
-        if j < weight:
-            dp[i][j] = dp[i-1][j]
-        else:
-            dp[i][j] = max(dp[i-1][j],dp[i-1][j-weight] + value)
-            
-print(dp[n][k])
+# print(item)
+
+for w,v in item:
+    for i in range(K,w-1,-1):
+   
+        dp[i] = max(dp[i],dp[i-w] + v)
+    #     print(dp,"!@")
+    # print("---")
+print(dp[K])
+
+
+# # 이전 상태 복사 방식 / 이전상태의 dp테이블을 복사해서 그걸 참조해서 한번만
+# for w, v in item:
+#     old_dp = dp[:]  # 이전 상태 복사
+#     for i in range(w, K + 1):  # 정방향 가능
+#         dp[i] = max(dp[i], old_dp[i - w] + v)
